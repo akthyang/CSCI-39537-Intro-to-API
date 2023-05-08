503d762c5ada0f69b04f12d91da73d16dfdb8f4d
akthyang <akthyang@gmail.com>
2023-05-08 17:12:31 -0400

    class code for customer api

---
+++ MyFirstAPI/MyFirstAPI/Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MyFirstAPI.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public Email? Email { get; set; }
+    }
+}
---
+++ MyFirstAPI/MyFirstAPI/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MyFirstAPI.Models
+{
+    public class Email
+    {
+        public int EmailId { get; set; }
+        public string EmailAddress { get; set; } = string.Empty;
+        public bool isSubscribed { get; set; }
+    }
+}
